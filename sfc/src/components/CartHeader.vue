3a954ccaed76c0416534eee3ef443aeadcb88690
노승은 <seun2230@gmail.com>
2021-09-15 13:40:09 +0900

    SFC

---
+++ sfc/src/components/CartHeader.vue
@@ -0,0 +1,30 @@
+<template>
+    <h1>{{ upperTitle }}</h1>
+</template>
+
+<script>
+
+import CartHeader from './components/CartHeader';
+export default {
+    data: () => {
+        return {
+            title: "Vue Cart"
+        }
+    },
+
+    computed: {
+        // 미리 계산 수행, 결과를 캐싱
+        upperTitle: function() {
+            return this.title.toUpperCase();
+        }
+    }
+}
+</script>
+
+<style scoped>
+    h1 {
+        font-size: 2.0em;
+        color: blue;
+    }
+
+</style>
